8a8a7f1089bccfae82ccc70b75eb580a709a6483
kwhitejr <kwhitejr.edu@gmail.com>
2015-11-28 12:41:21 -1000

    working blood distribution

--- Be-Positive/js/Be-Positive.js
+++ Be-Positive/js/Be-Positive.js
@@ -1,5 +1,5 @@
 BloodType = {
-  
+
   AB_POS : "AB_POS",
   AB_NEG : "AB_NEG",
   A_POS  : "A_POS",
@@ -12,16 +12,16 @@ BloodType = {
 };
 
 BloodTransfusionRules = {
-  
+
   /**
    * Set the simulation speed.
    * @type {Number} : Valid values between 1 and 200
    */
-  simulation_speed : 90,
+  simulation_speed : 200,
 
   /**
    * returns BloodType, or false to give no BloodType
-   * 
+   *
    * @name receive_patient
    * @param {Bank} blood_inventory
    * @param {Patient} patient
@@ -31,7 +31,7 @@ BloodTransfusionRules = {
    *   gender : String, (MALE,FEMALE)
    *   blood_type : String (BloodType)
    * }
-   * 
+   *
    * Bank properties {
    *   AB_POS : Integer,
    *   AB_NEG : Integer,
@@ -42,19 +42,93 @@ BloodTransfusionRules = {
    *   O_POS  : Integer,
    *   O_NEG  : Integer
    * }
-   * 
+   *
    */
 
   receive_patient : function (blood_inventory, patient) {
-    
-    // give a random blood type to anyone
-    // very bad idea!
-    return [
-      BloodType.AB_POS,
-      BloodType.AB_NEG,
-      BloodType.A_POS,
-      BloodType.A_NEG
-    ][Math.floor(Math.random()*4)];
+
+    var reNEG = /NEG$/;
+
+    if (patient.blood_type === BloodType.O_NEG) {
+      return BloodType.O_NEG || false;
+    }
+    if (patient.blood_type === BloodType.O_POS) {
+      return BloodType.O_POS || BloodType.O_NEG || false;
+    }
+    if (patient.blood_type === BloodType.A_NEG) {
+      return BloodType.A_NEG || BloodType.O_NEG || false;
+    }
+    if (patient.blood_type === BloodType.B_NEG) {
+      return BloodType.B_NEG || BloodType.O_NEG || false;
+    }
+    if (patient.blood_type === BloodType.A_POS) {
+      var bloodArrAPos = ['A_POS', 'A_NEG', 'O_POS', 'O_NEG'];
+      var greatestA = -1;
+      var greatestTypeA;
+      for (var i = 0; i < bloodArrAPos.length; i++) {
+        if (blood_inventory[bloodArrAPos[i]] > greatestA) {
+          greatestA = blood_inventory[bloodArrAPos[i]];
+          greatestTypeA = bloodArrAPos[i];
+        }
+      }
+      if (greatestA > 0) {
+        return BloodType[greatestTypeA];
+      } else {
+        return false;
+      }
+    }
+    if (patient.blood_type === BloodType.B_POS) {
+      var bloodArrBPos = ['B_POS', 'B_NEG', 'O_POS', 'O_NEG'];
+      var greatestB = -1;
+      var greatestTypeB;
+      for (var m = 0; m < bloodArrBPos.length; m++) {
+        if (blood_inventory[bloodArrBPos[m]] > greatestB) {
+          greatestB = blood_inventory[bloodArrBPos[m]];
+          greatestTypeB = bloodArrBPos[m];
+        }
+      }
+      if (greatestB > 0) {
+        return BloodType[greatestTypeB];
+      } else {
+        return false;
+      }
+    }
+    if (patient.blood_type === BloodType.AB_NEG) {
+      var greatest = -1;
+      var greatestBloodType;
+      for (var key in blood_inventory) {
+        if (key === 'O_NEG') {
+          continue;
+        } else if (reNEG.test(key)) {
+          // debugger;
+          if (blood_inventory[key] > greatest) {
+            greatest = blood_inventory[key];
+            greatestBloodType = key;
+          }
+        }
+      }
+      if (greatest > 0) {
+        return BloodType[greatestBloodType];
+      } else if (blood_inventory.O_NEG > 0) {
+        return BloodType.O_NEG;
+      } else {
+        return false;
+      }
+    }
+
+    if (patient.blood_type === BloodType.AB_POS) {
+      var greatest2 = -1;
+      var greatestBloodType2;
+      for (var key2 in blood_inventory) {
+        if (blood_inventory[key2] > greatest2) {
+          greatest2 = blood_inventory[key2];
+          greatestBloodType2 = key2;
+        }
+      }
+      if (greatest2 > 0) {
+        return BloodType[greatestBloodType2] || false;
+      }
+    }
 
   }
 
